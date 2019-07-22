a76e7d3cee97838812a2cb28303de4e21d5d9b89
Natalie Taktachev <ntaktach@gmail.com>
2019-07-22 01:34:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,21 +23,17 @@ wow = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
 array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  rray = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[5]
 end
 
 def update_element_from_index(array, index_number, element)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
 array[4] = "totally"
 end
